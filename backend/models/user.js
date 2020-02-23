62d4d130d3242129b0ee179e7d59580706c25902
Phonekham <phone.madridista@gmail.com>
2020-02-23 12:06:33 +0700

    User schema virtual fields and methods

--- backend/models/user.js
+++ backend/models/user.js
@@ -55,4 +55,40 @@ const userSchema = new Schema(
   { timestamp: true }
 );
 
+userSchema
+  .virtual("password")
+  .set(function(password) {
+    // Create a temporary variable called_password
+    this._password = password;
+    // Generate salt
+    this.salt = this.makeSalt();
+    // Encrypt Password
+    this.hashed_password = this.encryptPassword(password);
+  })
+  .get(function() {
+    return this._password;
+  });
+
+userSchema.methods = {
+  authenticate: function(plainText) {
+    return this.encryptPassword(plainText) === this.hashed_password;
+  },
+
+  encryptPassword: function(password) {
+    if (!password) return "";
+    try {
+      return crypto
+        .createHmac("sha1", this.salt)
+        .update(password)
+        .digest("hex");
+    } catch (err) {
+      return "";
+    }
+  },
+
+  makeSalt: function() {
+    return Math.round(new Date().valueOf() * Math.random()) + "";
+  }
+};
+
 module.exports = mongoose.model("User", userSchema);
