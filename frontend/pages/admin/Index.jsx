cb07b59a85e5e6100a1651a284f17021a3afdd9e
Phonekham <phone.madridista@gmail.com>
2020-02-26 10:56:03 +0700

    Category page

--- frontend/pages/admin/Index.jsx
+++ frontend/pages/admin/Index.jsx
@@ -7,7 +7,23 @@ const AdminIndex = () => {
     <div>
       <Layout>
         <Admin>
-          <h2>Admin Dashboard</h2>
+          <div className="container-fluid">
+            <div className="row">
+              <div className="col-md-12 pt-5 pb-5">
+                <h2>Admin Dashboard</h2>
+              </div>
+              <div className="col-md-4">
+                <ul class="list-group">
+                  <li className="list-group-item">
+                    <Link href="/admin/crud/category-tag">
+                      <a>Create Category</a>
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+              <div className="col-md-8">right</div>
+            </div>
+          </div>
         </Admin>
       </Layout>
     </div>
