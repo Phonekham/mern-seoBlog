16b1dd00a62af1fe0fa5ffa2356bc76add42db0c
Phonekham <phone.madridista@gmail.com>
2020-03-01 11:36:40 +0700

    Blog create form

--- frontend/components/crud/CreateBlog.js
+++ frontend/components/crud/CreateBlog.js
@@ -12,12 +12,66 @@ const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
 import "../../node_modules/react-quill/dist/quill.snow.css";
 
 const CreateBlog = ({ router }) => {
-  return (
-    <div>
-      <h2>create blog form</h2>
-      {JSON.stringify(router)}
-    </div>
-  );
+  const [body, setBody] = useState({});
+  const [values, setValues] = useState({
+    error: "",
+    sizeError: "",
+    success: "",
+    formData: "",
+    title: "",
+    hidePublishButton: false
+  });
+  const {
+    error,
+    sizeError,
+    success,
+    formData,
+    title,
+    hidePublishButton
+  } = values;
+
+  const publishBlog = e => {
+    e.preventDefault();
+    console.log("ready to publish");
+  };
+
+  const handleChange = name => e => {
+    return console.log(e.target.value);
+  };
+
+  const handleBody = e => {
+    return console.log(e);
+  };
+
+  const createBlogForm = () => {
+    return (
+      <form onSubmit={publishBlog}>
+        <div className="form-group">
+          <label className="text-muted">Title</label>
+          <input
+            value={title}
+            type="text"
+            className="form-control"
+            onChange={handleChange("title")}
+          ></input>
+        </div>
+        <div className="form-group">
+          <ReactQuill
+            value={body}
+            placeholder="Write somthing..."
+            onChange={handleBody}
+          ></ReactQuill>
+        </div>
+        <di>
+          <button type="submit" className="btn btn-primary">
+            Publish
+          </button>
+        </di>
+      </form>
+    );
+  };
+
+  return <div>{createBlogForm()}</div>;
 };
 
 export default withRouter(CreateBlog);
