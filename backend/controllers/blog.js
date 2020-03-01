47d033b5c9018c4f2d29e1829e6caad51499917b
Phonekham <phone.madridista@gmail.com>
2020-03-01 10:30:16 +0700

    Create blog with categories and tags

--- backend/controllers/blog.js
+++ backend/controllers/blog.js
@@ -18,6 +18,28 @@ exports.create = (req, res) => {
       });
     }
     const { title, body, categories, tags } = fields;
+
+    if (!title || !title.length) {
+      return res.status(400).json({
+        error: "Title is required"
+      });
+    }
+    if (!body || body.length < 200) {
+      return res.status(400).json({
+        error: "Content is too short"
+      });
+    }
+    if (!categories || !categories.length === 0) {
+      return res.status(400).json({
+        error: "atleast one category is required"
+      });
+    }
+    if (!tags || !tags.length === 0) {
+      return res.status(400).json({
+        error: "atleast one tag is required"
+      });
+    }
+
     let blog = new Blog();
     blog.title = title;
     blog.body = body;
@@ -25,6 +47,9 @@ exports.create = (req, res) => {
     blog.mtitle = `${title} | ${process.env.APP_NAME}`;
     blog.mdesc = stripHtml(body.substring(0, 160));
     blog.postedBy = req.user._id;
+    // categories and tags
+    let arrayOfCategories = categories && categories.split(",");
+    let arrayOfTags = categories && tags.split(",");
     if (files.photo) {
       if (files.photo.size > 10000000) {
         return res.status(400).json({
@@ -40,7 +65,31 @@ exports.create = (req, res) => {
           error: errorHandler(err)
         });
       }
-      res.json(result);
+      Blog.findByIdAndUpdate(
+        result._id,
+        { $push: { tags: arrayOfTags } },
+        { new: true }
+      ).exec((err, result) => {
+        if (err) {
+          return res.status(400).json({
+            error: errorHandler(err)
+          });
+        } else {
+          Blog.findByIdAndUpdate(
+            result._id,
+            { $push: { categories: arrayOfCategories } },
+            { new: true }
+          ).exec((err, result) => {
+            if (err) {
+              return res.status(400).json({
+                error: errorHandler(err)
+              });
+            } else {
+              res.json(result);
+            }
+          });
+        }
+      });
     });
   });
 };
