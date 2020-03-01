e4a339ed902a7d1efe0c0358b8a3e6f4932cad88
Phonekham <phone.madridista@gmail.com>
2020-03-01 11:19:22 +0700

    Blog create action and imports

--- frontend/components/crud/CreateBlog.js
+++ frontend/components/crud/CreateBlog.js
@@ -1,9 +1,23 @@
-const CreateBlog = () => {
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import Router from "next/router";
+import dynamic from "next/dynamic";
+import { withRouter } from "next/router";
+import { getCookie, isAuth } from "../../actions/auth";
+import { getTags } from "../../actions/tag";
+import { getCategories } from "../../actions/category";
+import { createBlog } from "../../actions/blog";
+
+const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
+import "../../node_modules/react-quill/dist/quill.snow.css";
+
+const CreateBlog = ({ router }) => {
   return (
     <div>
       <h2>create blog form</h2>
+      {JSON.stringify(router)}
     </div>
   );
 };
 
-export default CreateBlog;
+export default withRouter(CreateBlog);
--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,8 @@
-module.exports = {
+const withCSS = require("@zeit/next-css");
+module.exports = withCSS({
   publicRuntimeConfig: {
     APP_NAME: "SEOBLOG",
     API_DEVELOPMENT: "http://localhost:8000/api",
     PRODUCTION: false
   }
-};
+});
