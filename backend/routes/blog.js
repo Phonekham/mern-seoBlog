c5bb3039bb29050f47b7b6f1e06cf7da55dcc296
Phonekham <phone.madridista@gmail.com>
2020-03-04 10:46:16 +0700

    API Related Blog

--- backend/routes/blog.js
+++ backend/routes/blog.js
@@ -8,7 +8,8 @@ const {
   read,
   remove,
   update,
-  photo
+  photo,
+  listRelated
 } = require("../controllers/blog.js");
 
 router.post("/blog", requireSignin, adminMiddleware, create);
@@ -18,5 +19,6 @@ router.get("/blog/:slug", read);
 router.delete("/blog/:slug", requireSignin, adminMiddleware, remove);
 router.put("/blog/:slug", requireSignin, adminMiddleware, update);
 router.get("/blog/photo/:slug", photo);
+router.post("/blogs/related", listRelated);
 
 module.exports = router;
