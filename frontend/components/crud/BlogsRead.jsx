786abae747730bf8c86f78e1a2ad1464977f4050
Phonekham <phone.madridista@gmail.com>
2020-03-04 11:54:31 +0700

    list and delete blog

--- frontend/components/crud/BlogsRead.jsx
+++ frontend/components/crud/BlogsRead.jsx
@@ -3,11 +3,69 @@ import { useEffect, useState } from "react";
 import Router from "next/router";
 import { getCookie, isAuth } from "../../actions/auth";
 import { list, removeBlog } from "../../actions/blog";
+import moment from "moment";
 
 const BlogRead = () => {
+  const [blogs, setBlogs] = useState([]);
+  const [message, setMessage] = useState("");
+
+  const token = getCookie("token");
+
+  useEffect(() => {
+    loadBlogs();
+  }, []);
+
+  const loadBlogs = () => {
+    list().then(data => {
+      if (data.error) {
+        console.log(data.error);
+      }
+      setBlogs(data);
+    });
+  };
+
+  const deleteConfirm = slug => {
+    let answer = window.confirm("Are u sure to delete?");
+    if (answer) {
+      removeBlog(slug, token).then(data => {
+        if (data.error) {
+          console.log(data.error);
+        } else {
+          setMessage(data.message);
+          loadBlogs();
+        }
+      });
+    }
+  };
+
+  const showAllBlogs = () => {
+    return blogs.map((blog, i) => {
+      return (
+        <div key={i} className="pb-5">
+          <h3>{blog.title}</h3>
+          <p className="mark">
+            Written by {blog.name} | Published on{" "}
+            {moment(blog.updatedAt).fromNow()}
+          </p>
+          <button
+            className="btn btn-sm btn-danger"
+            onClick={() => deleteConfirm(blog.slug)}
+          >
+            Delete
+          </button>
+        </div>
+      );
+    });
+  };
+
   return (
     <React.Fragment>
-      <p>update delete blog</p>
+      <div className="row">
+        <div className="col-md-12">
+          {message && <div className="alert alert-warning">{message}</div>}{" "}
+          {showAllBlogs()}
+        </div>
+      </div>
     </React.Fragment>
   );
 };
--- frontend/pages/admin/crud/blogs.js
+++ frontend/pages/admin/crud/blogs.js
@@ -7,7 +7,7 @@ const Blogs = () => {
   return (
     <Layout>
       <Admin>
-        <div className="container-fluid">
+        <div className="container">
           <div className="row">
             <div className="col-md-12 pt-5 pb-5">
               <h2>List Blogs</h2>
