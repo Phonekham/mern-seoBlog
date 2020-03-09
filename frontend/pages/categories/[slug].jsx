85a0ac2c9862bd4abc3a6909bbe47798e118986c
Phonekham <phone.madridista@gmail.com>
2020-03-09 10:47:29 +0700

    categories & tags SEO

--- frontend/pages/categories/[slug].jsx
+++ frontend/pages/categories/[slug].jsx
@@ -1,11 +1,42 @@
 import Head from "next/head";
-import Link from "next/link";
 import Layout from "../../components/Layout";
 import { getCategory } from "../../actions/category";
-import { API, APP_NAME, DOMAIN, FB_APP_ID } from "../../config";
+import { APP_NAME, DOMAIN, FB_APP_ID } from "../../config";
 import Card from "../../components/blog/Card";
 
-const Category = ({ category, blogs }) => {
+const Category = ({ category, blogs, query }) => {
+  const head = () => (
+    <Head>
+      <title>
+        {category.name} | {APP_NAME}
+      </title>
+      <meta
+        name="description"
+        content={`Best programming tutorials on ${category.name}`}
+      />
+      <link rel="canonical" href={`${DOMAIN}/categories/${query.slug}`} />
+      <meta property="og:title" content={`${category.name}| ${APP_NAME}`} />
+      <meta
+        property="og:description"
+        content={`Best programming tutorials on ${category.name}`}
+      />
+      <meta property="og:type" content="webiste" />
+      <meta property="og:url" content={`${DOMAIN}/categories/${query.slug}`} />
+      <meta property="og:site_name" content={`${APP_NAME}`} />
+
+      <meta
+        property="og:image"
+        content={`${DOMAIN}/static/images/seoblog.jpg`}
+      />
+      <meta
+        property="og:image:secure_url"
+        content={`${DOMAIN}/static/images/seoblog.jpg`}
+      />
+      <meta property="og:image:type" content="image/jpg" />
+      <meta property="fb:app_id" content={`${FB_APP_ID}`} />
+    </Head>
+  );
+
   return (
     <React.Fragment>
       <Layout>
--- frontend/pages/tags/[slug].jsx
+++ frontend/pages/tags/[slug].jsx
@@ -1,12 +1,45 @@
 import Head from "next/head";
-import Link from "next/link";
 import Layout from "../../components/Layout";
 import { getTag } from "../../actions/tag";
+import { APP_NAME, DOMAIN, FB_APP_ID } from "../../config";
 import Card from "../../components/blog/Card";
 
-const Tag = ({ tag, blogs }) => {
+const Tag = ({ tag, blogs, query }) => {
+  const head = () => (
+    <Head>
+      <title>
+        {tag.name} | {APP_NAME}
+      </title>
+      <meta
+        name="description"
+        content={`Best programming tutorials on ${tag.name}`}
+      />
+      <link rel="canonical" href={`${DOMAIN}/categories/${query.slug}`} />
+      <meta property="og:title" content={`${tag.name}| ${APP_NAME}`} />
+      <meta
+        property="og:description"
+        content={`Best programming tutorials on ${tag.name}`}
+      />
+      <meta property="og:type" content="webiste" />
+      <meta property="og:url" content={`${DOMAIN}/categories/${query.slug}`} />
+      <meta property="og:site_name" content={`${APP_NAME}`} />
+
+      <meta
+        property="og:image"
+        content={`${DOMAIN}/static/images/seoblog.jpg`}
+      />
+      <meta
+        property="og:image:secure_url"
+        content={`${DOMAIN}/static/images/seoblog.jpg`}
+      />
+      <meta property="og:image:type" content="image/jpg" />
+      <meta property="fb:app_id" content={`${FB_APP_ID}`} />
+    </Head>
+  );
+
   return (
     <React.Fragment>
+      {head()}
       <Layout>
         <main>
           <div className="container-fluid text-center">
@@ -32,7 +65,9 @@ Tag.getInitialProps = ({ query }) => {
     } else {
       return {
         tag: data.tag,
-        blogs: data.blogs
+        blogs: data.blogs,
+        query,
+        query
       };
     }
   });
